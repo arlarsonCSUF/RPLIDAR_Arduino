5a5bead53815089658ecf2d953f4a0f17c38c9a3
Alex Larson <arlarson@crimson.ua.edu>
2016-02-19 13:40:28 -0800

    added minValue to Sector type

--- Sector.h
+++ Sector.h
@@ -1,11 +1,13 @@
 typedef struct{
-  uint32_t sum = 0;
-  uint32_t samples = 0;
-  float avgDistance = 0;
+  float sum = 0;             // used to calculate average
+  uint32_t samples = 0;      // number of samples taken in sector
+  float avgDistance = 0;     // avg = sum/samples
+  uint16_t minVal = 0xFFFF;  // the closest point in the sector, set to max value if there are no points in sector
 } Sector;
 
 void sectorReset(Sector *s){
   s->sum = 0;
   s->samples = 0;
   s->avgDistance = 0;
+  s->minVal = 0xFFFF;
 }
